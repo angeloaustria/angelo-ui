9f0198190ddf9982e987dd175c216deae110b205
angeloaustria <angelogabriel.austria@gmail.com>
2022-04-01 20:39:05 -0400

    Add more button styling and props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import "./Button.css";
+import { ClassNames } from '@emotion/react';
+import "./Button.scss";
 
 export interface ButtonProps {
   label: string;
+  large?: boolean;
+  small?: boolean;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return (
+    <ClassNames>
+      {({ cx }) => (
+        <button className={cx("angelo-ui-button", { "angelo-ui-small": props.small }, { "angelo-ui-large": props.large })}>
+          {props.label}
+        </button>
+      )}
+    </ClassNames>
+  );
 };
 
 export default Button;
