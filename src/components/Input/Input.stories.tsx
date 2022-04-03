7181763e35fb0622e49ecad0b78adcac9eaff18b
angeloaustria <angelogabriel.austria@gmail.com>
2022-04-03 10:55:43 -0400

    Initial commit for Input component

---
+++ src/components/Input/Input.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Input from "./Input";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Input",
+  component: Input,
+} as ComponentMeta<typeof Input>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const ClickMe = Template.bind({});
+ClickMe.args = {
+  disabled: false,
+  placeholder: "Enter message",
+  value: "Hello world"
+};
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,30 @@
+import React, { InputHTMLAttributes } from "react";
+import { ClassNames } from "@emotion/react";
+import "./Input.scss";
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  fullWidth?: boolean;
+  large?: boolean;
+  small?: boolean;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+  const { fullWidth, small, large, ...otherInputProps } = props;
+  return (
+    <ClassNames>
+      {({ cx }) => (
+        <input
+          className={cx(
+            "angelo-ui-input",
+            { "angelo-ui-full-width": props.fullWidth },
+            { "angelo-ui-small": props.small },
+            { "angelo-ui-large": props.large }
+          )}
+          {...otherInputProps}
+        />
+      )}
+    </ClassNames>
+  );
+};
+
+export default Input;
